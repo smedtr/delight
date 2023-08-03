0f238a951a6fcf7e3c9ba92584df03e38d4a64fe
smedtr <smedtr@email.com>
2023-08-03 16:31:30 +0200

    Home view

--- djangodelights/inventory/views.py
+++ djangodelights/inventory/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+from django.views.generic.base import TemplateView
+from .models import Ingredient, MenuItem, Purchase, RecipeRequirement
+
 # Create your views here.
+
+class HomeView(TemplateView):
+    # 
+    template_name = "inventory/home.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = context = super().get_context_data(*args, **kwargs)
+        context['menu_items'] = MenuItem.objects.all()
+        context['ingredients'] = Ingredient.objects.all()
+        context['purchases'] = Purchase.objects.all()        
+        return context
+
